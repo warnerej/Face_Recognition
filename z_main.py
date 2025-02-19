0b6ae89e30131adb3bf1ad1a3fbce15f2f3a8542
warnerej <warnerej@mail.uc.edu>
2025-02-19 17:08:35 -0500

    Adds Loop of Program

--- z_main.py
+++ z_main.py
@@ -3,27 +3,31 @@ import matching_face as mf
 import os
 
 directory = "known_faces"
+face_checking = True
 
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-user_status = int(input("Are you a new user (1) or an existing user (2): "))
+while face_checking:
+    user_status = int(input("Are you a new user (1) or an existing user (2): "))
 
-user_name = input("Enter your name: ")
+    user_name = input("Enter your name: ")
 
-if (user_status == 1):
-    print("Line your face up in the center of the camera and click s when you are ready")
-    sf.scan_face(user_name)
-    print("You are now in our system!")
-elif (user_status == 2):
-    verification = mf.match_face()
+    if (user_status == 1):
+        print("Line your face up in the center of the camera and click s when you are ready")
+        sf.scan_face(user_name)
+        print("You are now in our system!")
+    elif (user_status == 2):
+        verification = mf.match_face()
 
-    if (verification[0]):
+        if (verification[0]):
 
-        if (verification[1].upper() == user_name.upper()):
-            print(f"We have your face in our system: {verification[1]}")
+            if (verification[1].upper() == user_name.upper()):
+                print(f"We have your face in our system: {verification[1]}")
 
+            else:
+                print(f"We have your face in our system but that is the wrong name")
         else:
-            print(f"We have your face in our system but that is the wrong name")
-    else:
-        print("You aren't in our systems")
+            print("You aren't in our systems")
+    
+    face_checking = int(input("Would you like to continue: Yes(1) No(0): "))
